c53519c95be365c8372755668da2b10e9c47d4d5
Natallia <natalyasafarevich@gmail.com>
2026-07-03 14:13:54 +0200

    C-1 feat: add auth-sidebar

--- shared/ui/logo/Logo.tsx
+++ shared/ui/logo/Logo.tsx
@@ -7,18 +7,16 @@ type Props = {
 export const Logo = ({ variant = "dark" }: Props) => {
   const logoVariant = variant === "light" ? "text-white" : "text-black";
   return (
-    <>
-      <Text
-        component={"a"}
-        href="/"
-        variant="text"
-        className={`${logoVariant} flex items-center w-max`}
-      >
-        <span className="uppercase rounded-lg font-bold mr-2.5 w-7.5 h-7.5 flex items-center justify-center bg-white text-brand-dark">
-          rs
-        </span>
-        <span className="font-bold">Recommendation Club</span>
-      </Text>
-    </>
+    <Text
+      component={"a"}
+      href="/"
+      variant="text"
+      className={`${logoVariant} flex items-center w-max`}
+    >
+      <span className="uppercase rounded-lg font-bold mr-2.5 w-7.5 h-7.5 flex items-center justify-center bg-white text-brand-dark">
+        rs
+      </span>
+      <span className="font-bold">Recommendation Club</span>
+    </Text>
   );
 };
--- widgets/auth-sidebar/ui/AuthSidebar.tsx
+++ widgets/auth-sidebar/ui/AuthSidebar.tsx
@@ -1,9 +1,22 @@
 import { Logo } from "@/shared/ui/logo/Logo";
+import { Text } from "@/shared/ui/text/Text";
 
 export const AuthSidebar = () => {
   return (
-    <div className="bg-brand-dark p-7.5">
+    <div className="bg-brand-dark p-7.5 text-white h-screen flex flex-col justify-between">
       <Logo variant="light" />
+      <div>
+        <Text variant="title" className="mb-3 mt-3">
+          Recommendations you can trust.
+        </Text>
+        <Text className="mb-3 mt-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          magnam dolore facilis. Cumque asperiores minus laborum tempora
+          voluptas, veritatis harum sint amet similique commodi repudiandae quod
+          ut!
+        </Text>
+      </div>
+      <Text className="text-[#faf9f77f]">©2026 Recommendation Club</Text>
     </div>
   );
 };
