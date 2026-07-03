b0ad8e5231c904636f5afbe2dbcd4e384c1ca528
Natallia <natalyasafarevich@gmail.com>
2026-07-03 14:07:10 +0200

    C-1 feat: add logo component

--- shared/ui/logo/Logo.tsx
+++ shared/ui/logo/Logo.tsx
@@ -8,10 +8,17 @@ export const Logo = ({ variant = "dark" }: Props) => {
   const logoVariant = variant === "light" ? "text-white" : "text-black";
   return (
     <>
-      <Text component={"a"} href="/" className="text-amber-600"></Text>
-      <Link className={`${logoVariant} font-bold font-siz`} href={"/"}>
-        Recommendation Club
-      </Link>
+      <Text
+        component={"a"}
+        href="/"
+        variant="text"
+        className={`${logoVariant} flex items-center w-max`}
+      >
+        <span className="uppercase rounded-lg font-bold mr-2.5 w-7.5 h-7.5 flex items-center justify-center bg-white text-brand-dark">
+          rs
+        </span>
+        <span className="font-bold">Recommendation Club</span>
+      </Text>
     </>
   );
 };
--- shared/ui/text/Text.tsx
+++ shared/ui/text/Text.tsx
@@ -1,24 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
 
 type Props<T extends ElementType> = {
   children: ReactNode;
   component?: T;
   variant?: "title" | "text";
+  className?: string;
 } & Omit<ComponentPropsWithoutRef<T>, "component">;
+
 export const Text = <T extends ElementType = "p">({
   component: Component = "p" as T,
   children,
   variant = "text",
+  className,
   ...rest
 }: Props<T>) => {
   const variantClasses = {
     title: "text-2xl font-bold",
     text: "text-sm font-normal",
   };
-
   return (
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    <Component className={`${variantClasses[variant]}`} {...(rest as any)}>
+    <Component
+      className={`${variantClasses[variant]} ${className ?? ""}`}
+      {...(rest as any)}
+    >
       {children}
     </Component>
   );
